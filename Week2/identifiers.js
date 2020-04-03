02c14c7ae0cfc497ba6ab1069a224465a4dd3572
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-04-03 04:02:51 +0300

    started exercise

---
+++ Week2/identifiers.js
@@ -0,0 +1,31 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "hyfuser",
+    password: "hyfpassword",
+    database: "week2"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE IF NOT EXISTS employee (employee_no INT AUTO_INCREMENT PRIMARY KEY, full_name VARCHAR(255), salary BIGINT(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+
+    con.query("SET @@global.sql_mode= '';", function (err, result) {
+        if (err) throw err;
+        console.log("Query about strict mode updated!");
+    })
+
+    con.query("ALTER TABLE employee ADD COLUMN manager VARCHAR(255);", function (err, result) {
+        if (err) throw err;
+        console.log("Manager Column Added");
+    })
+
+    //adding a foreing key which is gonna BE managers column
+
+});
